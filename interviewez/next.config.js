8d21a39a2ff5a02415c445be043681c96ab652af
RyanCampbell <Rpcampbell434@icloud.com>
2023-03-23 18:12:20 -0400

    Create nextjs app and basic routing to home page
    Getting basics of the app set up. Setting up the main view as /home with the intent of creating subnavs for the main parts of the app.

---
+++ interviewez/next.config.js
@@ -0,0 +1,19 @@
+const { default: next } = require('next')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
