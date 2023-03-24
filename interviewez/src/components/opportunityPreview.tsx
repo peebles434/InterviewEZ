16bc907e08c562773e9589fe73a1b462a4ade207
RyanCampbell <Rpcampbell434@icloud.com>
2023-03-23 21:47:12 -0400

    Add preview card basic output placeholders

--- interviewez/src/components/opportunityPreview.tsx
+++ interviewez/src/components/opportunityPreview.tsx
@@ -1,11 +1,34 @@
 "use client"
 
-import Paper from '@mui/material/Paper'
+import * as React from 'react';
+import Card from '@mui/material/Card'
 import styles from '../app/page.module.css'
+import { CardContent, Typography } from '@mui/material';
 
+const card = (
+    <React.Fragment>
+        <CardContent>
+            <Typography variant='h6' component='div'>
+                COMPANY - ROLE
+            </Typography>
+            <Typography variant='body2' component='div'>
+                STATUS
+            </Typography>
+            <Typography variant='body2' component='div'>
+                LOCATION
+            </Typography>
+            <Typography variant='body2' component='div'>
+                RECRUITER
+            </Typography>
+            <Typography variant='body2' component='div'>
+                NEXT INTERVIEW DATE
+            </Typography>
+        </CardContent>
+    </React.Fragment>
+)
 
 export default function OpportunityPreview() {
   return (
-    <Paper elevation={3} className={styles.opportunityCard}/>
+    <Card elevation={3} className={styles.opportunityCard}>{card}</Card>
   )
 }
